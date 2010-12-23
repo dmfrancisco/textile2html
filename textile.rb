8c3340bf2116c47337b3d846972891b3af6cff10
David Francisco <dfrancisco@ipn.pt>
2010-12-23 11:08:33 +0000

    github version (same as heroku but without the .gems and config.ru files)

---
+++ textile.rb
@@ -0,0 +1,39 @@
+
+require 'sinatra'
+require 'erb'
+require 'open-uri'
+
+set :public, File.dirname(__FILE__) + '/public'
+
+get '/' do
+  erb :index
+end
+
+
+helpers do
+
+  # Get the page following the question mark
+  def get_url(urltext = request.url)
+    return "" unless urltext.include? "?"
+    page = urltext.split('?').last
+    return page
+  end
+
+  # Convert \n, \t and other meta characters
+  def parse(content)
+    content.gsub('"', '\"').gsub("\t", "\\t").gsub("\n", "\\n").gsub("\r", "\\r")
+  end
+
+  # Get the content of the textile remote file
+  def get_content(url)
+    open(url) do |src|
+      return parse(src.read)
+    end
+    rescue
+      return ""
+  end
+end
+
+# How to run Sinatra
+# $ gem install sinatra
+# $ ruby -rubygems textile.rb
